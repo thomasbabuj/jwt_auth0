97306e58fb1c6576ac26b9e91e2e236ed102d6cd
Thomas Babu <thomas@interuptive.com>
2016-04-16 12:10:53 +0800

    defined the api

--- main.go
+++ main.go
@@ -14,5 +14,16 @@ func main() {
 	// serving static assets like images, css from the /static/{file} route
 	r.PathPrefix("/static").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 
+	//Defining API routes
+	r.Handle("/status", NotImplemented).Methods("GET")
+	r.Handle("/products", NotImplemented).Methods("GET")
+	r.Handle("/products/{slug}/feedback", NotImplemented).Methods("POST")
+
 	http.ListenAndServe(":3000", r)
 }
+
+// NotImplemented Handler, whenever an API is hit we will simply return
+// the message "Not Implemented"
+var NotImplemented = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Not Implemented"))
+})
